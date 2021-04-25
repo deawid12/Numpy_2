c0d0b2f490d6ab76d4c5a37145d78f989a824026
Dawid <dawid_plocharczyk3@o2.pl>
2021-04-25 19:38:43 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import numpy as np
+# # Zad 1
+# # Utwórz dwie macierze 1x3 różnych wartości a następnie wykonaj operację mnożenia.
+#
+# a = np.array([2,4,6])
+# b = np.array([3,5,7])
+# c = a * b
+# print(c)
+
+# # Zad 2
+# # Utwórz macierz 3x3 oraz 4x4 i znajdź najniższe wartości dla każdej kolumny i każdego rzędu.
+#
+# d = np.array([[11,2,33,4],[12,44,1,0],[19,3,5,9],[8,7,6,5]])
+# e = np.array([[1,2,3],[2,3,4],[5,6,7]])
+# print(d.sum(axis=0))
+# print(d.min(axis=1))
+#
+# print(e.sum(axis=0))
+# print(e.min(axis=1))
+
+# # Zad 3
+# # Wykorzystaj macierze z zadania pierwszego i wykonaj iloczyn macierzy.
+# print(a.dot(b))
+
+# Zad 4
+# # Utwórz dwie macierze 1x3 gdzie pierwsza z nich będzie zawierała liczby całkowite, a druga liczby rzeczywiste. Następnie wykonaj na nich operację mnożenia.
+# m1 = np.array([1,2,3])
+# m2 = np.array([0.1,0.2,0.3])
+# m = m1*m2
+# print(m)
+
+# # Zad 5
+# # Utwórz macierz 2x3 różnych wartości a następnie wylicz sinus dla każdej z jej wartości i zapisz do zmiennej “a”.
+# l = np.array([[1,2],[3,4],[5,6]])
+# a = []
